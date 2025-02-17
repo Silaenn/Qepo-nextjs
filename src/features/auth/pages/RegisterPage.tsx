34b7a5630ab0efa83c26416484194d2cdae55b8e
Deo silaen <deokeldisilaen@gmail.com>
2025-02-17 14:25:44 +0700

    add updateRegisterPage

---
+++ src/features/auth/pages/RegisterPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { PageContainer } from "~/components/layout/PageContainer";
+
+const RegisterPage = () => {
+  return (
+    <PageContainer>
+      <main>
+        <h1>RegisterPage</h1>
+      </main>
+    </PageContainer>
+  );
+};
+
+export default RegisterPage;
