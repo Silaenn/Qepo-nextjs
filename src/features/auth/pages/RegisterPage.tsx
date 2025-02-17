f426e79200467b9d7e5a7d3bdff3f9452334a7cf
Deo silaen <deokeldisilaen@gmail.com>
2025-02-17 22:48:47 +0700

    add updateRegisterPage

--- src/features/auth/pages/RegisterPage.tsx
+++ src/features/auth/pages/RegisterPage.tsx
@@ -1,12 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { PageContainer } from "~/components/layout/PageContainer";
+import { SectionContainer } from "~/components/layout/SectionContainer";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "~/components/ui/card";
+import { Checkbox } from "~/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { registerFormSchema, type RegisterFormSchema } from "../forms/register";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
 
 const RegisterPage = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const form = useForm<RegisterFormSchema>({
+    resolver: zodResolver(registerFormSchema),
+  });
+
+  const onRegisterSubmit = (values: RegisterFormSchema) => {
+    alert("register");
+  };
+
   return (
     <PageContainer>
-      <main>
-        <h1>RegisterPage</h1>
-      </main>
+      <SectionContainer className="flex min-h-[calc(100vh-144px)] w-full flex-col justify-center">
+        <Card className="w-full max-w-[480px] self-center">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold text-primary">Buat Akun</h1>
+            <p className="text-muted-foreground">
+              Qepoin kreator favorite kamu
+            </p>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onRegisterSubmit)}
+                className="flex flex-col gap-y-1"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input type="email" {...field} />
+                      </FormControl>
+                      <FormDescription />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type={showPassword ? "text" : "password"}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription />
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Label className="mt-4 flex items-center gap-2">
+                  <Checkbox
+                    checked={showPassword}
+                    onCheckedChange={(checked) => setShowPassword(!!checked)}
+                  />
+                  Show Password
+                </Label>
+
+                <Button className="mt-4 w-full">Buat Akun</Button>
+              </form>
+            </Form>
+
+            {/* {CONTINUE WITH GOOGLE} */}
+          </CardContent>
+          <CardFooter className="flex flex-col gap-4">
+            <div className="flex w-full items-center justify-between gap-x-4">
+              <div className="h-[2px] w-full border-t-2" />
+              <p className="flex-1 text-nowrap text-sm text-muted-foreground">
+                Atau lanjut dengan
+              </p>
+              <div className="h-[2px] w-full border-t-2" />
+            </div>
+
+            <Button variant="secondary" className="w-full" size="lg">
+              <FcGoogle />
+              Buat Akun dengan Google
+            </Button>
+
+            <p>
+              Sudah punya akun?{" "}
+              <Link href="/login" className="font-bold text-blue-800">
+                P, Login
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </SectionContainer>
     </PageContainer>
   );
 };
